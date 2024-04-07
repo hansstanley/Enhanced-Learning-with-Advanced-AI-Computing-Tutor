779458aee2033c19c81a4c018cf384c77b6a14f1
hansstanley <nusstuhss@gmail.com>
2024-04-08 00:33:56 +0800

    add ui frame

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,42 @@
-import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { NextUIProvider } from "@nextui-org/react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+  Image,
+  Input,
+  NextUIProvider,
+} from "@nextui-org/react";
+import { FaArrowUp } from "react-icons/fa6";
 
 function App() {
   return (
     <NextUIProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="flex flex-row justify-center h-screen p-8 gap-8">
+        <Card className="max-w-2xl w-full h-full">
+          <CardHeader className="flex gap-2">
+            <Image alt="logo" width={40} height={40} radius="sm" src={logo} />
+            <p className="text-base">CS Tutor</p>
+          </CardHeader>
+          <Divider />
+          <CardBody></CardBody>
+          <Divider />
+          <CardFooter className="flex flex-row gap-2">
+            <Input isClearable type="text" placeholder="Ask anything" />
+            <Button isIconOnly color="primary">
+              <FaArrowUp />
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card className="max-w-2xl w-full h-full">
+          <CardHeader>
+            <p className="text-base">PDF viewer</p>
+          </CardHeader>
+        </Card>
       </div>
     </NextUIProvider>
   );
