8808a1158839f446aaa25a7307c589d857375961
daniellingzy <e0773883@u.nus.edu>
2024-04-10 17:57:56 +0800

    new branch

--- backend.py
+++ backend.py
@@ -1,9 +1,26 @@
-import langchain
-from langchain_community.llms import Ollama
+import os
+import chromadb
+from tqdm import tqdm
+from langchain_community.chat_models import ChatOllama
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.tools.retriever import create_retriever_tool
 
-loader = PyPDFLoader("example_data/layout-parser-paper.pdf")
-pages = loader.load_and_split()
+llm = ChatOllama(model="llama2:7b")
+emb = OllamaEmbeddings(model="llama2:7b")
 
-
-llm = Ollama(model="llama2")
+# Define retrieval tool
+retriever
+retriever_tool = create_retriever_tool(
+    retriever,
+    "textbook_search",
+    "Search for information about LangSmith. For any questions about LangSmith, you must use this tool!",
+)
