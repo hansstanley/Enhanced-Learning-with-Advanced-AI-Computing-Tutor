767e43ac1f7187f0e413fe3983bb8e241195bdb1
hansstanley <nusstuhss@gmail.com>
2024-04-15 21:51:29 +0800

    add tutor with history and rag

--- tutors.py
+++ tutors.py
@@ -1,13 +1,24 @@
 from typing import AsyncIterable, Iterable
 
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.history_aware_retriever import (
+    create_history_aware_retriever,
+)
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain.globals import set_debug
 from langchain_community.chat_message_histories.in_memory import (
     ChatMessageHistory,
 )
 from langchain_community.chat_models.ollama import ChatOllama
+from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.messages import AIMessageChunk
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables import RunnableGenerator, RunnablePassthrough
 from langchain_core.runnables.history import RunnableWithMessageHistory
+
+import helpsheet_retriever
+
+set_debug(True)
 
 
 class TutorBase:
@@ -42,3 +53,85 @@ class TutorBase:
     async def _aparse_stream(self, chunks: AsyncIterable[AIMessageChunk]):
         async for chunk in chunks:
             yield {"answer": chunk.content}
+
+
+class TutorWithHistoryRAG(TutorBase):
+    def __init__(
+        self,
+        model="llama2:7b",
+        multi_query=False,
+        compress=False,
+    ) -> None:
+        self.llm = ChatOllama(model=model)
+        self.store = {}
+
+        retriever = helpsheet_retriever.get_retriever(
+            multi_query=multi_query,
+            compress=compress,
+        )
+
+        # Contextualize question
+        contextualize_q_system_prompt = """You are given chat history and \
+        the latest user question which might reference the chat history. \
+        If needed, formulate a standalone question that captures both \
+        the chat history and the question. \
+        Otherwise, return the question as is. \
+        Refrain from answering the question."""
+        contextualize_q_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", contextualize_q_system_prompt),
+                MessagesPlaceholder("chat_history"),
+                ("human", "{input}"),
+            ]
+        )
+        history_aware_retriever = create_history_aware_retriever(
+            self.llm, retriever, contextualize_q_prompt
+        )
+
+        qa_system_prompt = """You are a Computer Science tutor \
+        for question-answering tasks.
+        For questions about definition and concept, \
+        be as concise as possible and answer in at most 5 sentences.
+        You may use your own knowledge, but state clearly when you do.
+        Do NOT mention "the context".
+        Do NOT answer questions in the context.
+        If you cannot answer, do not know the answer, or think \
+        the questions is irrelevant to data structures or algoritms, \
+        state clearly and say "Please check with your tutor." \
+        Refrain from making up an answer.
+
+        Context: ###
+        {context}
+        ###
+        """
+        qa_prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", qa_system_prompt),
+                MessagesPlaceholder(variable_name="chat_history"),
+                ("human", "{input}"),
+            ]
+        )
+        question_answer_chain = create_stuff_documents_chain(
+            self.llm,
+            qa_prompt,
+        )
+        rag_chain = create_retrieval_chain(
+            history_aware_retriever,
+            question_answer_chain,
+        )
+
+        self.chain = RunnableWithMessageHistory(
+            rag_chain,
+            self._get_session_history,
+            input_messages_key="input",
+            history_messages_key="chat_history",
+            output_messages_key="answer",
+        )
+
+    def clear_history(self):
+        self.store.clear()
+
+    def _get_session_history(self, session_id: str) -> BaseChatMessageHistory:
+        if session_id not in self.store:
+            self.store[session_id] = ChatMessageHistory()
+        return self.store[session_id]
